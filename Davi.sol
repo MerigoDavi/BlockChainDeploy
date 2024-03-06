a07963fc067e40b2ed3103aff72ad340877422e9
Davi de Oliveira Ferreira <158862432+Baptela@users.noreply.github.com>
2024-03-06 17:49:24 -0300

    Update Davi.sol

--- Davi.sol
+++ Davi.sol
@@ -8,3 +8,30 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
+contract DaviDeOliveiraFerreira is ERC20, ERC20Burnable, ERC20Pausable, Ownable, ERC20Permit {
+    constructor(address initialOwner)
+        ERC20("DaviDeOliveiraFerreira", "DF")
+        Ownable(initialOwner)
+        ERC20Permit("DaviDeOliveiraFerreira")
+    {}
+function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(address from, address to, uint256 value)
+        internal
+        override(ERC20, ERC20Pausable)
+    {
+        super._update(from, to, value);
+    }
+}
